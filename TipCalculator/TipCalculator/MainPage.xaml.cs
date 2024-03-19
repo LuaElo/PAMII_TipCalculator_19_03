2e96043bcdc19f588fe76b41f8dca12e161cc2d8
Luara e Eloah <eloahluaraa123@gmail.com>
2024-03-19 12:46:13 -0300

    Aula Pam -  CALCULADORA

---
+++ TipCalculator/TipCalculator/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿namespace TipCalculator
+{
+    public partial class MainPage : ContentPage
+    {
+     
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonPercento15_Clicked(object sender, EventArgs e)
+        {
+       
+        }
+
+        private void ButtonPercento30_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonUp_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonDown_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PercentagesSlider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            PorcenLabel.Text = $"{PercentagesSlider.Value.ToString ()}%";     
+        }
+    }
+
+}
