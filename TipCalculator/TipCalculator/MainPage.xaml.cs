7dbdb05d426e11a23800ad02e6420b4f553ab438
EloahLuara <eloahluaraa123@gmail.com>
2024-03-26 12:46:22 -0300

    calculadora

--- TipCalculator/TipCalculator/MainPage.xaml.cs
+++ TipCalculator/TipCalculator/MainPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿namespace TipCalculator
+﻿using CoreBluetooth;
+using IntentsUI;
+
+namespace TipCalculator
 {
     public partial class MainPage : ContentPage
     {
@@ -11,21 +14,37 @@
 
         private void ButtonPercento15_Clicked(object sender, EventArgs e)
         {
-       
+            PercentagesSlider.Value = 15;
         }
 
         private void ButtonPercento30_Clicked(object sender, EventArgs e)
         {
+            PercentagesSlider.Value = 30;
 
         }
 
         private void ButtonUp_Clicked(object sender, EventArgs e)
         {
-
+            double tip = CalculateTip();
+            tip = Math.Ceiling(tip);
+            TigLabel.Text = Convert.ToString("C2");
+            double valort = tip + Convert.ToDouble(ValueEntry.Text);
+            TotalLabel.Text = valort.ToString("C2");
         }
 
         private void ButtonDown_Clicked(object sender, EventArgs e)
         {
+            
+        }
+
+        private double CalculateTip()
+        {
+            double valorT = Convert.ToDouble(ValueEntry.Text);
+            double porcntagem = PercentagesSlider.Value;
+            double valorGorgeta = valorT * (porcntagem / 100) ;
+            return (valorGorgeta);
+
+
 
         }
 
